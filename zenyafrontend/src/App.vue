8cd1e1451888f440fd09a245c74ea9c6113d6efe
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2022-03-22 10:18:29 +0100

    Add Bootstrap & VueRouter

--- zenyafrontend/src/App.vue
+++ zenyafrontend/src/App.vue
@@ -6,6 +6,15 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 export default {
   name: 'App',
--- zenyafrontend/src/components/HelloWorld.vue
+++ zenyafrontend/src/components/HelloWorld.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="hello">
-    <img style="width: 15%" alt="Vue logo" src="../assets/Zenya.png"><br/>
     <iframe src="https://docs.google.com/document/d/e/2PACX-1vTEp1e_xQFfV1SXnE2loEbD2wF40_XT1Mk9VRQRpURM7u5oNp5VYztIXaLzd4_wC_lmBoesP8Mz4tnV/pub?embedded=true"></iframe>
+    <div>
+      <b-button>Button</b-button>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
+    </div>
+  
   </div>
+  
 </template>
 
 <script>
