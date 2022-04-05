e84533108bd51ac899951c76e28f78ba9640a1aa
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2022-04-05 13:56:29 +0200

    Add guy voorbeeldje aan frontend

--- zenyafrontend/src/components/SignInForm.vue
+++ zenyafrontend/src/components/SignInForm.vue
@@ -1,43 +1,49 @@
 <template>
 
 <div class="d-flex justify-content-center align-items-center container">
-  <div class="row">    
-      <button v-on:click="ConnectAccounts()" type="submit" class="btn btn-primary">Log in</button>
-      
-  </div>
-  {{this.msg}}     
+  <h3 v-if="this.user">User: {{this.user}}</h3><br/>
+
+  <GoogleLogin :params="params" :renderParams="renderParams" :onSuccess="onSuccess"></GoogleLogin>
+  <GoogleLogin class="googleButton" :params="params" :logoutButton="true">Logout</GoogleLogin>
+
+  
 </div>
 
   
 </template>
 
 <script>
-import axios from 'axios'
-export default {
-  name: 'SignInForm',
-  props: {
-
-  },
-  data() {
-    return {
-      msg: "",
-
-    };
-  },
-
-
-  methods:{
-    ConnectAccounts: function () {
-      axios
-      .post('https://localhost:7112/connectaccounts/connectaccounts')
-      .then(response => {
-        this.msg = "Response " + response.status
-      }).catch(error => {
-        this.msg =  error
-      })
-    }  
+import GoogleLogin from 'vue-google-login';
+  export default {
+    name: 'App',
+    data() {
+      return {
+        // client_id is the only required property but you can add several more params, full list down bellow on the Auth api section
+        params: {
+            client_id: "408797503550-r3ag70nqn6uhrbcadfs1a59pf29jrmfs.apps.googleusercontent.com"
+        },
+        user: null,
+        // only needed if you want to render the button with the google ui
+        renderParams: {
+            width: 250,
+            height: 50,
+            longtitle: true
+        }
+      }
+    },
+    components: {
+        GoogleLogin
+    },
+    methods: {
+        onSuccess(googleUser) {
+            console.log(googleUser);
+ 
+            // This only gets the user information: id, name, imageUrl and email
+            console.log(googleUser.getBasicProfile());
+            this.user = googleUser.getBasicProfile().yv;
+        }
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -51,23 +57,31 @@ export default {
   flex-direction: column;
 }
 
-img {
-  width: 20%;
-  margin: 24px;
+.googleButton {
+  padding: 8px;
+  margin: 12px;
+  border: none;
+  border-radius: 1px;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 25%);
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  -webkit-transition: background-color .218s,border-color .218s,box-shadow .218s;
+  transition: background-color .218s,border-color .218s,box-shadow .218s;
+  -webkit-user-select: none;
+  -webkit-appearance: none;
+  background-color: #fff;
+  background-image: none;
+  color: #262626;
+  cursor: pointer;
+  outline: none;
+  overflow: hidden;
+  position: relative;
+  text-align: center;
+  vertical-align: middle;
+  white-space: nowrap;
+  width: auto;
+  color: #757575;
+  background-color: #fff;
 }
 
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
--- zenyafrontend/src/views/SignIn.vue
+++ zenyafrontend/src/views/SignIn.vue
@@ -7,11 +7,123 @@
 
 <script>
 import SignInForm from "@/components/SignInForm.vue";
+import ajax from "vuejs-ajax"
+
+
 
 export default {
+  
   name: "SignIn",
   components: {
     SignInForm
+  },
+  methods: {
+    ZenyaAPI: function(config){
+      /// <summary>Helper class for call iProva API's</summary>
+      /// <param name="iProvaUrl" type="string">URL of iProva</param>
+      /// <param name="config" type="object">Client configuration object</param>
+      /// <returns></returns>
+
+      this._iProvaURL = config.iProvaUrl;
+      this._logonMethod = config.logonMethod;
+      this._alwaysGetNewUserToken = config.alwaysGetNewUserToken;
+      this._version = config.version;
+      this._apiKey = config.apiKey;
+
+      if (config.logonMethod === this.ZenyaAPI.Enumerations.LogonMethod.WindowsAuthentication && !config.apiKey)
+        throw new Error('When using WindowsAuthentication an api key must be used.')
+
+      //append trailing / if needed
+      if (this._iProvaURL[this._iProvaURL.length - 1] != "/")
+        this._iProvaURL += "/";
+
+      //initiate token helper
+      if (this._logonMethod !== this.ZenyaAPI.Enumerations.LogonMethod.None)
+        this._tokenHelper = new this.ZenyaAPI.TokenHelper(this._iProvaURL, this._logonMethod);
+
+      return this;
+    },
+
+    _ajaxError: function (jqxhr, textStatus, errorThrown) {
+      /// <summary>Handles jQuery ajax request error</summary>
+      /// <param name="jqxhr" type="jqXHR">jQuery jqXHR object, which is a superset of the XMLHTTPRequest object. (http://api.jquery.com/jQuery.ajax/#jqXHR)</param>
+      /// <param name="textStatus" type="string">Possible values are "timeout", "error", "abort", and "parsererror"</param>
+      /// <param name="errorThrown" type="string">textual portion of the HTTP status</param>
+      /// <returns></returns>
+
+      this.ZenyaAPI.showAjaxError(jqxhr, textStatus, errorThrown, '@@textStatus@@ during API Call.')
+    },
+
+    callREST: function (callParameters, forceNewToken) {
+      var apiCallFunction = function (callParameters, token)
+      {
+        ajax({
+          method: callParameters.method,
+          url: this._iProvaURL + callParameters.path,
+          crossDomain: true,
+          xhrFields: {
+            withCredentials: (this._logonMethod === this.ZenyaAPI.Enumerations.LogonMethod.Cookie)
+          },
+          beforeSend: function (request)
+          {
+            //if a token is set, set the token in authorization header
+            if (token)
+              request.setRequestHeader('Authorization', 'token ' + token);
+
+            if (this._version)
+              request.setRequestHeader("x-api-version", this._version);
+
+            if (this._apiKey)
+              request.setRequestHeader("x-api_key", this._apiKey);
+          }.bind(this),
+          contentType: "application/json",
+          data: (callParameters.parameters ? JSON.stringify(callParameters.parameters) : null),
+          success: function (result)
+          {
+            callParameters.callback(result);
+          },
+          error: function (jqxhr, textStatus, errorThrown)
+          {
+            //token expired?
+            if (jqxhr.responseJSON && jqxhr.responseJSON.ErrorCode == 1014)
+              this.callREST(callParameters, true);
+            else
+              this._ajaxError(jqxhr, textStatus, errorThrown);
+
+          }.bind(this)
+        });
+      }.bind(this, callParameters);
+
+        //get token first? (winauth / saml variants)
+        if (this._logonMethod >= this.ZenyaAPI.Enumerations.LogonMethod.WindowsAuthentication)
+          {
+          if (arguments.length == 1)
+            forceNewToken = false;
+            forceNewToken = (forceNewToken || this.alwaysGetNewUserToken);
+            this._tokenHelper.getToken(apiCallFunction, forceNewToken);
+        }
+        else //direct API call
+        {
+          apiCallFunction();
+        }
+    },
+
+    tokenHelper: function (iProvaUrl, logonMethod) {
+      /// <summary>TokenHelper constructor</summary>
+      /// <param name="iProvaUrl" type="string">URL of iProva (with trailing /)</param>
+      /// <param name="logonMethod" type="iProvaAPI.Enumerations.LogonMethod">Login method to use to identify user.</param>
+      /// <returns></returns>
+
+      this._iProvaURL = iProvaUrl;
+      this._logonMethod = logonMethod;
+      this._token = null;
+
+      return this;
+    },
+
+    getToken(){
+      
+    }
   }
 };
 
